98d127c027cd7fd19dc1519df1571b54a5476c0c
Eljah <eljah@mail.ru>
2018-02-17 08:12:37 +0300

    adding prototype scope in java config

--- 01. Introduction to Spring/src/main/java/ru/kpfu/itis/ConfigExample.java
+++ 01. Introduction to Spring/src/main/java/ru/kpfu/itis/ConfigExample.java
@@ -1,13 +1,16 @@
 package ru.kpfu.itis;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class ConfigExample {
 
 
     @Bean
+    //@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public Guitar guitar() {
         return new Guitar();
     }
